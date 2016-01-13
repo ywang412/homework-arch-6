96850346dbc8a2432109d7cec14baa0497c6b729
kongsys <kongsys@163.com>
2016-01-13 21:22:27 +0800

    Queue

--- 01/kongsys/StackAndQueue.py
+++ 01/kongsys/StackAndQueue.py
@@ -19,3 +19,23 @@ class Stack:
             del self._list[-1]
             self._size -= 1
             return elem
+
+
+class Queue:
+    
+    def __init__(self, *base):
+        self._list = list(base)
+        self._size = len(self._list)
+
+    def push(self, elem):
+        self._list.append(elem)
+        self._size += 1
+
+    def pop():
+        if self._size < 1:
+            return None
+        else:
+            elem = self._list[0]
+            del self._list[0]
+            self._list -= 1
+            return elem
