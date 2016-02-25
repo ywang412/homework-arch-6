fa0bd21821ab398aac84f7b63512cb4c5509b77d
lyao <ly36843@163.com>
2016-02-25 11:38:52 +0800

    03

--- 03/liaoyao/getimg.py
+++ 03/liaoyao/getimg.py
@@ -6,6 +6,9 @@ import re
 
 
 class GetImg():
+	def __init__(self):
+		self.href = {}
+		self.img = {}
 
 	def gethtml(self,url):
 		headers = {"User-Agent":"ss"}
@@ -13,24 +16,44 @@ class GetImg():
 		response = urllib2.urlopen(req)
 		return response.read()
 
-	def gethref(self,html,count=None):			#截取http://www.douban.com/people/xxxxx,使用dict更合适
+	"""def gethref(self,html,count=None):			#截取http://www.douban.com/people/xxxxx,使用dict更合适
 		href_list = []
 		match_str = re.compile(r'<a href=".*"\sclass="nbg".*</a>')
 		match = re.findall(match_str,html)
 		for line in match:
 			href = line.split()[1].split('"')[1]
 			href_list.append(href)
-		return href_list
-		
+		return href_list"""
+	
+	def imgurl(self,html):
+		img_str = re.compile('<img src="(.*?)"\sclass="m_sub_img"')
+		match_img = re.findall(img_str,html)
+		for img in match_img:
+			self.img[img] = 1
+
+	def gethref(self,html,count=None):			#截取http://www.douban.com/people/xxxxx,使用dict更合适
+		self.imgurl(html)
+		match_str = re.compile(r'<a href=".*"\sclass="nbg".*</a>')
+		match = re.findall(match_str,html)
+		while count > 1:
+			for line in match:
+				href = line.split()[1].split('"')[1]
+				tmp = self.gethtml(href)
+				self.gethref(tmp)
+			count -= 1 
+		return self.img
+
 
 if __name__ == "__main__":
 	url = 'http://www.douban.com/people/ahbei/'
 	con = GetImg()
 	html = con.gethtml(url)
-	href_list = con.gethref(html)   
-	print href_list
+	img_dict = con.gethref(html,5)   
+	for k,v in img_dict.items():
+		img_name = k.split('/')[-1]
+		with file(img_name,'w') as f:
+			f.write(con.gethtml(k))	
 
-	#for href in href_list:
-	#	print href	
-	#	print "lllll"
-	#print href_list
+
+
+
